6f3c419caf43d3918c0bd6264ea7e29b268944c9
meakif <mehmet61875@gmail.com>
2022-08-17 20:46:08 +0300

    second

---
+++ for.js
@@ -0,0 +1,21 @@
+// * ============================================
+// *            DONGULER(LOOPS)- FOR
+// * ============================================
+
+console.log("****** FOR *******");
+
+//? Ornek: 1 den n kadar sayiları toplayan kodu yaziniz.
+
+const n = Number(prompt("Bir sayi girniz:"));
+
+let toplam = 0;
+for (let i = 1; i <= n; i++) {
+  // toplam = toplam + i;
+  toplam += i;
+  console.log(toplam, i);
+}
+
+console.log("SONUC:", toplam);
+
+//? Ornek: 0-100 Arasinda 10 adet rasgele sayi ureten kodu
+//? for dongusu ile yaziniz..
